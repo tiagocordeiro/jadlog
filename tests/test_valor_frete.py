f05465d083164890a4b65baf3d4210ca64a7d087
Tiago Cordeiro <tiago@mulhergorila.com>
2018-03-29 12:17:53 -0300

    Adicionado Testes para calculo de frete rodoviário e expresso

--- tests/test_valor_frete.py
+++ tests/test_valor_frete.py
@@ -2,9 +2,9 @@ from jadlog import frete_rodoviario, frete_expresso
 
 
 def test_frete_rodoviario():
-    frete = frete_rodoviario(72, 44, 62, 27, '09210700', '48602575', 2450)
+    frete = frete_rodoviario(72, 44, 62, 27, '09220700', '48602575', 2450)
     assert frete == "215,46"
 
 def test_frete_expresso():
-    frete = frete_expresso(72, 44, 62, 27, '09210700', '09210700', 2450)
+    frete = frete_expresso(72, 44, 62, 27, '09220700', '09210700', 2450)
     assert frete == "112,48"
